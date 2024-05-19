b0bb1c66779aa0d2c6d937076892238b0c7dad80
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-05-19 18:53:02 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': {'min':'450px','max':'639px'},
+        '2xs': {'min':'320px','max':'449px'},
+      },
+      fontFamily: {
+        Poppins: ["var(--font-poppins)", ...fontFamily.sans],
+        code: "var(--font-code)",
+        grotesk: "var(--font-grotesk)",
+        Poetsen_One: ["Poetsen One"],
+        Concert: ["Concert One"],
+      },
+    },
   },
   plugins: [],
 }
